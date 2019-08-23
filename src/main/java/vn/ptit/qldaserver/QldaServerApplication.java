6f52d77a286282864bbfc7f720da1fc8326f3c12
ninedyseven <ninedyseven@gmail.com>
2019-08-23 18:20:50 +0700

    first commit

---
+++ src/main/java/vn/ptit/qldaserver/QldaServerApplication.java
@@ -0,0 +1,13 @@
+package vn.ptit.qldaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QldaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QldaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/ptit/qldaserver/QldaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package vn.ptit.qldaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QldaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
