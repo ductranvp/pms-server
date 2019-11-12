fa56cb2b9a90894df7cd304f4c27d704a093754d
ductv <ninedyseven@gmail.com>
2019-11-12 09:36:23 +0700

    update

--- src/main/java/vn/ptit/pms/repository/UserProjectRepository.java
+++ src/main/java/vn/ptit/pms/repository/UserProjectRepository.java
@@ -1,6 +1,7 @@
 package vn.ptit.pms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import vn.ptit.pms.domain.UserProject;
 import vn.ptit.pms.domain.enumeration.ProjectRole;
 import vn.ptit.pms.domain.key.UserProjectKey;
@@ -8,6 +9,10 @@ import vn.ptit.pms.domain.key.UserProjectKey;
 import java.util.List;
 
 public interface UserProjectRepository extends JpaRepository<UserProject, UserProjectKey> {
+
+    @Query(value = "SELECT * FROM user_project up " +
+            "JOIN project p ON up.project_id = p.id " +
+            "WHERE p.deleted = FALSE AND up.user_id = ?1", nativeQuery = true)
     List<UserProject> findByIdUserId(Long userId);
 
     List<UserProject> findByIdProjectId(Long projectId);
