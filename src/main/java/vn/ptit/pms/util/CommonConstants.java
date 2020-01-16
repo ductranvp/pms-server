a34f9d2e1543f58506656944bdf1f6bc062eb18f
ductv <ductv@rabiloo.com.vn>
2020-01-16 09:26:23 +0700

    update config

--- src/main/java/vn/ptit/pms/util/CommonConstants.java
+++ src/main/java/vn/ptit/pms/util/CommonConstants.java
@@ -1,9 +1,9 @@
 package vn.ptit.pms.util;
 
 public interface CommonConstants {
-    String CLIENT_BASE_URL = "http://localhost:3000";
+    String CLIENT_BASE_URL = "http://54.146.189.220:3000";
     String APP_PACKAGE = "vn.ptit.pms";
     String DEFAULT_LANGUAGE = "en";
-    String SAVED_ATTACHMENT_PATH = "D:\\pms\\attachment";
-    String SAVED_AVATAR_PATH = "D:\\pms\\avatar";
+    String SAVED_ATTACHMENT_PATH = "\\home\\pms\\attachment";
+    String SAVED_AVATAR_PATH = "\\home\\pms\\avatar";
 }
